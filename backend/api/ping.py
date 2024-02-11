d8c5e492e078dd2a1c7a95e0c51c5da2dbf5641b
andiricum2 <andiricum2@gmail.com>
2024-02-11 13:51:01 +0100

    Update ping.py

--- backend/api/ping.py
+++ backend/api/ping.py
@@ -1,8 +1,7 @@
-from flask import jsonify
 from flask import Blueprint
 
 ping_bp = Blueprint('ping', __name__)
 
 @ping_bp.route('/ping', methods=['GET'])
 def ping():
-    return jsonify({"message": "pong"})
+    return "pong"
